675a213914490ac3dceb62e56d9e1204796e8d27
Yarden15 <yarden15@gmail.com>
2019-11-15 16:54:00 +0200

    install project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>hello from react</h1>
-    </div>
-  );
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <h1>hello from react</h1>
+      </div>
+    );
+
+  }
 }
 
 export default App;
