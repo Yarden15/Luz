168e68593eda149aa546922ebc8fe32ae0afd87e
Yarin15 <yyar399@gmail.com>
2019-11-29 12:07:54 +0200

    rename calendar

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import Spinner from './components/layout/Spinner';
 import Navbar from './components/layout/Navbar';
-import Calander from './components/calander/Calander';
+import Calendar from './components/calander/Calendar';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
         <Navbar />
-        <Calander />
+        <Calendar />
       </div>
     );
   }
