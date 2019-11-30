ccb5492123cb2e2aeb543f29b58c522adaf1010f
Yarden15 <yarden15@gmail.com>
2019-11-30 10:23:50 +0200

    ...

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 //import Spinner from './components/layout/Spinner';
 import Navbar from './components/layout/Navbar';
-import Schedule from './components/calander/Schedule';
+import ScheduleItem from './components/calendar/ScheduleItem';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
         <Navbar />
-        <Schedule />
+        <ScheduleItem />
       </div>
     );
   }
