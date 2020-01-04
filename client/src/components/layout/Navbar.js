e71d1ecd7cb7b621a28ace1f2fb61be49900441a
Yarden15 <yarden15@gmail.com>
2020-01-04 18:02:50 +0200

    ..

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,25 +1,34 @@
 import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import AppLogo from '../../styles/assets/logo_white.png';
-
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 const Navbar = () => {
   // @todo - destruct isAuthenticated from the right component
   // for now its intialize by defaul to be false
   const isAuthenticated = true;
-
+  library.add(
+    faCoffee,
+    faCheckSquare
+  )
   //   @todo - create the correct list to this method
   //   The Navbar will look diffrent if the user is Authenticated to site
   const authLinks = (
     <Fragment>
       <li>
-        <Link to='/about'>שם המשתמש</Link>
+        <Link to='/'><FontAwesomeIcon icon="faCoffee" size="4x" />Home</Link>
       </li>
       <li>
-        <Link to='/user'>עדכון פרטים</Link>
+        <Link to='/user'>User Name</Link>
+      </li>
+      <li>
+        <Link to='/about'>About</Link>
       </li>
       <li>
         <i className='fas fa-sign-out-alt'>
-          <span className='hide-sm'>התנתק</span>
+          <span className='hide-sm'>Logout</span>
         </i>
       </li>
     </Fragment>
