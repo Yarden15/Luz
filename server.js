4ebbfa878a1269ee5ff7b92ffb61e830bdece01a
Yarden15 <yarden15@gmail.com>
2020-01-21 09:12:39 +0200

    .

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json({ extended: false }));
 
 // app.get('/', (req, res) => res.json({ msg: 'Welcome to Luz API...' }));
 
-app.get('*', (req,res) =>{
+app.get('/', (req,res) =>{
   res.sendFile(path.join(__dirname+'/client/public/index.html'));
 });
 
