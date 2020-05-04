7cd3cbb45039b677d8597839581f12648729ab86
Yarden15 <yarden15@gmail.com>
2020-05-04 21:41:53 +0300

    בדיקה

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import './styles/App.scss';
 import store from './store';
 import { Provider } from 'react-redux';
@@ -16,10 +16,10 @@ defineLang(lang[0] || 'en');
 
 ReactDOM.render(
   <Fragment>
-    <BrowserRouter />
-    <Provider store={store}>
-      <App />
-    </Provider>
-    <BrowserRouter />
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter >
   </Fragment>
   , document.getElementById('root'));
